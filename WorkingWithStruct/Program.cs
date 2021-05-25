42db19db0eb1f4eb8670a47e6ed2ad767ba3c7a0
Konstantin <kostik06@gmail.com>
2021-05-25 17:38:33 +0300

    mm

--- WorkingWithStruct/Program.cs
+++ WorkingWithStruct/Program.cs
@@ -20,9 +20,11 @@ namespace WorkingWithStruct
 
         static void Main(string[] args)
         {
-            int[] arr = { 3, 4, 5, 6, 7, 8 };
+            //int[] arr = { 3, 4, 5, 6, 7, 8 };
 
-            Console.WriteLine(SubsetXORSum(arr));
+            int arr = 1594322;
+
+            Console.WriteLine(IsPowerOfThree(arr));
 
             //Console.WriteLine(LastStoneWeight(arr));
         }
@@ -933,6 +935,58 @@ namespace WorkingWithStruct
             return result;
         }
 
+        #endregion
+
+        #region  Power of Two
+
+        //Given an integer n, return true if it is a power of two.Otherwise, return false.
+
+        //An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+
+
+        //Example 1:
+
+        //Input: n = 1
+        //Output: true
+        //Explanation: 20 = 1
+        //Example 2:
+
+        //Input: n = 16
+        //Output: true
+        //Explanation: 24 = 16
+        //Example 3:
+
+        //Input: n = 3
+        //Output: false
+        //Example 4:
+
+        //Input: n = 4
+        //Output: true
+        //Example 5:
+
+        //Input: n = 5
+        //Output: false
+
+
+
+        //Constraints:
+
+        //-231 <= n <= 231 - 1
+
+        public static bool IsPowerOfTwo(int n)
+        {
+            Console.WriteLine(n & (n - 1));
+            return (n > 0) && ((n & (n - 1)) == 0);
+        }
+
+        public static bool IsPowerOfThree(int n)
+        {
+            //double e = 1e-6;
+            //return (Math.Log(n) / Math.Log(3) + e) % 1 <= 2 * e;
+
+            return ((n > 0) && (1162261467 % n == 0));
+        }
         #endregion
 
     }
