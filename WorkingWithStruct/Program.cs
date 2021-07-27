64f569ad22589cf405a9a6dbf3531af543b6654b
dark1981ness <kostik06@gmail.com>
2021-07-27 21:44:47 +0300

    Substrings of Size Three with Distinct Characters

--- WorkingWithStruct/Program.cs
+++ WorkingWithStruct/Program.cs
@@ -3015,5 +3015,23 @@ namespace WorkingWithStruct
 
         #endregion
 
+        #region Substrings of Size Three with Distinct Characters
+
+        public static int CountGoodSubstrings(string s)
+        {
+            int result = 0;
+            for (int i = 1; i < s.Length - 1; i++)
+            {
+                if (s[i - 1] != s[i] && s[i] != s[i + 1] && s[i - 1] != s[i + 1])
+                    result++;
+            }
+
+            return result;
+
+        }
+
+
+        #endregion
+
     }
 }
